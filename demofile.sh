517648c3c4c5991c6de914ca2ff8c357d4ee913f
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-07-21 16:48:43 +0200

    wow dljfghsfj

---
+++ demofile.sh
@@ -0,0 +1,7 @@
+
+mkdir templates 
+python3 scaffold.py photos name pic author_id
+python3 scaffold.py video name vid author_id
+python3 scaffold.py post title content author_id
+python3 scaffold.py recording title rec author_id
+python3 scaffold.py user username email password sex photo country_id
