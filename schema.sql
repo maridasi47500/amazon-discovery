3574cd7044e4c8f50fbeb9ea4407c40cbb2c4f48
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-07-22 14:35:31 +0200

    wow dljfghsfj

--- schema.sql
+++ schema.sql
@@ -460,3 +460,36 @@ create table if not exists user(
             photo text,
             country_id text
                     );
+create table if not exists photos(
+        id integer primary key autoincrement,
+        name text,
+            pic text,
+            author_id text
+                    );
+create table if not exists video(
+        id integer primary key autoincrement,
+        name text,
+            vid text,
+            author_id text
+                    );
+create table if not exists post(
+        id integer primary key autoincrement,
+        title text,
+            content text,
+            author_id text
+                    );
+create table if not exists recording(
+        id integer primary key autoincrement,
+        title text,
+            rec text,
+            author_id text
+                    );
+create table if not exists user(
+        id integer primary key autoincrement,
+        username text,
+            email text,
+            password text,
+            sex text,
+            photo text,
+            country_id text
+                    );
